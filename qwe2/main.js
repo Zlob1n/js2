3fead459c6c03e7fd4bd27b37ab469db6d2fcc2d
Евгений Чернышов <113937693+Zlob1n@users.noreply.github.com>
2024-03-26 23:49:56 +0500

    Add files via upload

---
+++ qwe2/main.js
@@ -0,0 +1,70 @@
+// console.log(2002);
+// console.log('Hello, World!');
+// console.log('Hello,' +  ' World!');
+ // alert('Привет посетитель');
+ // console.log(" 'Добро' + 'пожаловать' + 'на курс' + 'Евгений' ");
+ // alert('20.03.2024');
+ // console.log(Infinity);
+ // console.log(10/0);
+ // console.log(NaN);
+// console.log('stroka' + 20);
+// console.log(NaN **0);
+ // BigInt в конце добавляем букву n и снимаем ограничения
+// console.log(78857463636363456474635467547436573n);
+// строковый
+// let str1 = 'Jojo'
+// let str2 = "Jojo 2"
+// let str3 = `Anime ${2 + 2 * 40}`
+// console.log(str1);
+// console.log(str2);
+// console.log(str3);
+// let check = true
+// let offcheck = false
+// console.log(4 > 1);
+// NULL пустое значение
+// console.log(null);
+// underfind значение не присвоено
+
+ // typeof - вывод типа данных
+ // console.log(typeof 42);
+ document.getElementById('one').innerHTML = '<h1> Hello World </h1>'
+ document.getElementById('one').innerHTML = 547
+ document.getElementById('one').innerHTML = 'Anime'
+let a = 'Evgen'
+ a = 437
+ console.log(a);
+ document.querySelector('#two').innerHTML = 865
+ document.querySelector('p').innerHTML = 'Хакуна Матата'
+ let inp = document.querySelector('.text')
+ let btn = document.querySelector('.btn')
+ let div = document.querySelector('.qwe')
+
+
+ btn.onclick = function(){
+    console.log('da');
+    let e = inp.value
+    div.innerHTML = e
+   inp.value = ''
+}
+
+// let k = prompt('Когда у тебя др?')
+// console.log(k);
+
+//if(условие){
+//    действие
+// }
+//if(k == 27){
+//   alert('Yes')
+//}
+//else{
+//    alert('No')
+// let k = prompt('Когда у тебя др?')}
+
+let l = prompt('Проверка числа')
+
+if (l > 100){
+    alert('Yes')
+}
+else {
+    console.log(l)
+}
